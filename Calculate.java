033caca562eb19c9ab3a49a8544e11826649b207
vv1rokr <vv1rokr@gmail.com>
2023-04-20 13:05:04 +0530

    Done

---
+++ Calculate.java
@@ -0,0 +1,18 @@
+package day1;
+//wap to print sum of two numbers
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		//int - datatype 
+		// a - variable name 
+		
+		int a=10;
+		int b=20;
+		int c= a+b;
+		
+		System.out.println(" sum of a & b="+ c);
+
+	}
+
+}

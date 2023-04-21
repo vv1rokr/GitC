5cd4d25638127f0b64195f4b5ddf2a8e7f6dcea3
vv1rokr <vv1rokr@gmail.com>
2023-04-21 10:42:55 +0530

    2nd Code Done

---
+++ AddMatrix.java
@@ -0,0 +1,27 @@
+package day7_2dArray;
+
+public class AddMatrix {
+
+	public static void main(String[] args) {
+		 int a[][]= {{10,20,30},
+				     {11,12,13}};
+		 
+	
+		 int b[][]= {{1,2,3},
+			        {4,5,6}};
+		 
+		 int c[][]= new int[2][3];
+		 
+		 for(int i=0;i<2;i++)
+		 {
+			 for(int j=0;j<3;j++)
+			 {
+				 c[i][j]= a[i][j]+ b[i][j];
+				 System.out.println("  "+ c[i][j]);
+			 }
+			 System.out.println();
+		 }
+
+	}
+
+}
